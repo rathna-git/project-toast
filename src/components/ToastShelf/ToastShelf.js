5ac09fd1bd956b8b4906f3ea008eae7686596a04
Rathna Chinthalapalli <hellorathnac@gmail.com>
2023-11-01 19:13:29 -0700

    fix: key error

--- src/components/ToastShelf/ToastShelf.js
+++ src/components/ToastShelf/ToastShelf.js
@@ -5,15 +5,12 @@ import styles from './ToastShelf.module.css';
 
 function ToastShelf({shelf}) {
   
-
-
-
   return (
     <ol className={styles.wrapper}>
       {shelf.map((item) => {
         return (
-          <li className={styles.toastWrapper}>
-            <Toast variant={item.variant}>{item.message}</Toast>
+          <li className={styles.toastWrapper} key={item.id}>
+            <Toast  variant={item.variant}>{item.message}</Toast>
           </li>
         )  
       })}
